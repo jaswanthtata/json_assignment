a7d11cfa7fe901d6b20f9638fda822ea88c361a0
Jaswanth <jaswanthtata22041995@gmail.com>
2017-05-10 10:55:01 +0530

    final commit

--- js/foodFactsJaswanth.js
+++ js/foodFactsJaswanth.js
@@ -1,69 +1,78 @@
+let log4js = require('log4js');
+let logger = log4js.getLogger();
 const fs = require('fs');
-module.exports = function convert(startYear) {
-  if(typeof startYear === 'string')
-  {
-    return '';
-  }
-  else if(typeof startYear !== 'number' || isNaN(startYear))
-  {
-       throw new Error('Not a number');
-  }
+module.exports = function convert(startYear)
+{
+	if(typeof startYear === 'string')
+	{
+		return '';
+	}
+	else if(typeof startYear !== 'number' || isNaN(startYear))
+	{
+		throw new Error('Not a number');
+	}
 
-const ln = require('readline').createInterface({
- input: fs.createReadStream('../inputdata/FoodFacts.csv')
-});
-const country = ['Netherlands', 'Canada', 'United Kingdom', 'United States',
- 'Australia', 'France', 'Germany', 'Spain', 'South Africa'];
-const data = [];
-const finalc = [];
-const sugarindex = 0;
-const saltindex = 0;
-const countryindex = 0;
-const countryv = 0;
-const sugar = 0;
-const salt = 0;
-const i = 0;
+	const ln = require('readline').createInterface({
+		input: fs.createReadStream('../inputdata/FoodFacts.csv')
+	});
+	const country = ['Netherlands', 'Canada', 'United Kingdom', 'United States',
+	'Australia', 'France', 'Germany', 'Spain', 'South Africa'];
+	const data = [];
+	const finalc = [];
+	const sugarindex = 0;
+	const saltindex = 0;
+	const countryindex = 0;
+	const countryv = 0;
+	const sugar = 0;
+	const salt = 0;
+	const i = 0;
+	const sugarv = Array(9).fill(0);
+	const saltv = Array(9).fill(0);
 
-const sugarv = Array(9).fill(0);
-const saltv = Array(9).fill(0);
+	ln.on('line', function (line)
+	{
+		data = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
 
-ln.on('line', function (line) {
-data = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+		while(i < 1)
+		{
+			countryindex = data.indexOf('countries_en');
+			sugarindex = data.indexOf('sugars_100g');
+			saltindex = data.indexOf('salt_100g');
+			i = i + 1;
+		}
 
-while(i < 1)
- {
-   countryindex = data.indexOf('countries_en');
-   sugarindex = data.indexOf('sugars_100g');
-   saltindex = data.indexOf('salt_100g');
-   i = i + 1;
- }
+		countryv = data[countryindex];
 
-countryv = data[countryindex];
+		sugar = data[sugarindex];
+		salt = data[saltindex];
+		if(salt === '')
+		{
+			salt = 0;
+		}
+		if(sugar === '')
+		{
+			sugar = 0;
+		}
 
-sugar = data[sugarindex];
-salt = data[saltindex];
-  if(salt === '') {
-   salt = 0;
-}
-  if(sugar === '') {
-   sugar = 0;
-}
+		const index = country.indexOf(countryv);
 
-const index = country.indexOf(countryv);
+		if(index !== -1)
+		{
+			sugarv[index] = sugarv[index] + parseFloat(sugar);
+			saltv[index] = saltv[index] + parseFloat(salt);
+		}
+	});
 
-    if(index !== -1) {
-     sugarv[index] = sugarv[index] + parseFloat(sugar);
-     saltv[index] = saltv[index] + parseFloat(salt);
-    }
-});
+	ln.on('close', function()
+	{
+		for(let h = 0; h < country.length; h = h + 1)
+		{
+			finalc.push({ Country: country[h], Sugar: sugarv[h], Salt: saltv[h]});
+		}
+		let finaljson = JSON.stringify(finalc);
+		fs.writeFile('../outputdata/outputFoodFactsJaswanth.json', finaljson);
+		logger.debug(finaljson);
+	});
 
-ln.on('close', function() {
- for(let h = 0; h < country.length; h = h + 1) {
-   finalc.push({ Country: country[h], Sugar: sugarv[h], Salt: saltv[h]});
-}
-
-fs.writeFile('../outputdata/foodFactsJaswanth.json', JSON.stringify(finalc));
-});
-
-return 'JSON written successfully';
+	return 'JSON written successfully';
 };
